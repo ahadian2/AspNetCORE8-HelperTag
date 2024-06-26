f327be7e272fd4f835db2d3877c914708a4617f2
Mohammadreza <ahadian2@gmail.com>
2024-06-26 14:32:14 +0330

    Add project files.

---
+++ HelperTag/Controllers/UserController.cs
@@ -0,0 +1,39 @@
+﻿using HelperTag.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HelperTag.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Person> persons = new List<Person>() {
+                  new Person { Id=1,Name="Mohammadreza",Family="Ahadian",Description="Description ...",Gender=true},
+                  new Person { Id=2,Name="Ahmadreza",Family="Jafari",Description="Description ...",Gender=true},
+                  new Person { Id=3,Name="Alireza",Family="Akbari",Description="Description ...",Gender=true},
+                  new Person { Id=4,Name="Hoda",Family="Mahmodi",Description="Description ...",Gender=false},
+                  new Person { Id=5,Name="Tanaz",Family="Rezaei",Description="Description ...",Gender=false}
+            };
+            return View(persons);
+        }
+        public IActionResult Info()
+        {
+            Person person = new Person() {
+                Id = 1,
+                Name = "Mohammadreza",
+                Family = "Ahadian",
+                Description = "Description ...",
+                Gender = false
+            };
+            return View(person);
+        }
+        [HttpPost]
+        public IActionResult Info(Person person)
+        {
+            if (!ModelState.IsValid) {
+                return View(person);
+            }
+            return View(person);
+        }
+    }
+}
---
+++ HelperTag/Models/Person.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HelperTag.Models
+{
+    public class Person
+    {
+        [Display(Name ="شماره یکتا")]
+        [Required(ErrorMessage = "فیلد شماره یکتا نمیتواند خالی باشد")]
+        public required int Id { get; set; }
+
+
+        [Display(Name = "نام")]
+        [Required(ErrorMessage ="فیلد نام نمیتواند خالی باشد")]
+        public string? Name { get; set; }
+        [Display(Name = "فامیلی")]
+        [Required(ErrorMessage = "فیلد فامیلی نمیتواند خالی باشد")]
+        public string? Family { get; set; }
+        [Display(Name = "توضیحات")]
+        public string? Description { get; set; }
+        [Display(Name = "جنسیت")]
+        public bool Gender { get; set; }
+    }
+}
